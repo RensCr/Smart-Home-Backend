977fb4f3c6bd53ea6450bb8b1f3dbe414918da3f
RensCr <rens.crijns@Outlook.com>
2024-09-26 11:26:32 +0200

    Database
    Database aanmaak met Entity Framework

---
+++ SmartHome-Backend/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartHome_Backend.Entities;
+namespace SmartHome_Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+        public DbSet<Gebruiker> Gebruikers { get; set; }
+    }
+    
+}
